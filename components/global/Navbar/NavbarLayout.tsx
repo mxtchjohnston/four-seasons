31d64eb72dcf70f984ec3b5e0b1293dd742cea69
Mitchell Stave <mitchjohnston99@gmail.com>
2024-01-11 17:25:56 -0800

    logo image

--- components/global/Navbar/NavbarLayout.tsx
+++ components/global/Navbar/NavbarLayout.tsx
@@ -1,8 +1,12 @@
+
 import Link from 'next/link'
 import Image from 'next/image'
 
 import { resolveHref, urlForImage } from '@/sanity/lib/utils'
 import type { MenuItem, SettingsPayload } from '@/types'
+
+import { urlForOpenGraphImage } from '@/sanity/lib/utils'
+
 
 interface NavbarProps {
   data: SettingsPayload
@@ -10,9 +14,10 @@ interface NavbarProps {
 export default function Navbar(props: NavbarProps) {
   const { data } = props
   const menuItems = data?.menuItems || ([] as MenuItem[])
+  const slug = "";
   return (
     <div className="sticky top-0 z-10 flex flex-wrap items-center gap-x-5 bg-white/80 px-4 py-4 backdrop-blur md:px-16 md:py-5 lg:px-32">
-      {/* <Image src={urlForImage(data.ogImage) ?? (require('@/app/icon.png'))} alt="logo" width={50} height={50} /> */}
+      <Image src={urlForOpenGraphImage(data.ogImage) ?? ''} alt="logo" width={200} height={100} />
       {menuItems &&
         menuItems.map((menuItem, key) => {
           const href = resolveHref(menuItem?._type, menuItem?.slug)
@@ -22,11 +27,18 @@ export default function Navbar(props: NavbarProps) {
           return (
             <Link
               key={key}
-              className={`text-lg hover:text-black md:text-xl ${
+              className={
+                `text-lg hover:text-black 
+                hover:underline transition-all duration-300 md:text-xl 
+                ${
                 menuItem?._type === 'home'
                   ? 'font-extrabold text-black'
                   : 'text-gray-600'
-              }`}
+                }
+                ${
+                  menuItem.slug === slug ? 'font-extrabold text-black' : ''
+                }
+              `}
               href={href}
             >
               {menuItem.title}
