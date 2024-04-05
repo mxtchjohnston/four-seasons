a789d48c881514e2f33db92813d0ba0d78977457
Mitchell Stave <mitchjohnston99@gmail.com>
2024-04-05 15:16:19 -0700

    errant import

--- components/shared/Paginated.tsx
+++ components/shared/Paginated.tsx
@@ -7,7 +7,6 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination"
-import { getImagesCount } from "@/sanity/loader/loadQuery"
 
 export function Paginated({ value }) {
   const perPage = value.perPage
